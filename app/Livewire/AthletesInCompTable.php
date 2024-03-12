1a99774cdd8b71e5437c041ca4ca443fdbb27eff
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-03-12 12:31:37 +0100

    Added athlete  info table added

---
+++ app/Livewire/AthletesInCompTable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Livewire;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Illuminate\Database\Eloquent\Builder;
+use App\Models\AthleteInComp;
+
+class AthletesInCompTable extends DataTableComponent
+{
+    //protected $model = AthleteInComp::class;
+    public $compId;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->hideIf(TRUE),
+            Column::make("Ime", "getAthlete.firstName")
+                ->sortable(),
+            Column::make("Prezime", "getAthlete.lastName")
+                ->sortable(),
+            Column::make("Spol", "getAthlete.gender")
+                ->sortable(),
+            Column::make("Adresa", "getAthlete.address")
+                ->sortable(),
+            Column::make("Grad", "getAthlete.city")
+                ->sortable(),
+            Column::make("Država", "getAthlete.state")
+                ->sortable(),
+            Column::make("Zip", "getAthlete.zip")
+                ->sortable(),
+            Column::make("Osobni broj", "getAthlete.athlete_id")
+                ->sortable(),
+        ];
+    }
+
+    public function builder(): Builder{
+        return AthleteInComp::query()
+            ->where('comp_id', $this->compId);
+    }
+}
--- resources/views/view/competition.blade.php
+++ resources/views/view/competition.blade.php
@@ -16,21 +16,23 @@
               <button class="nav-link active" id="basic-info-tab" data-bs-toggle="tab" data-bs-target="#basic-info-tab-pane" type="button" role="tab" aria-controls="basic-info-tab-pane" aria-selected="true">Osnovno</button>
             </li>
             <li class="nav-item" role="presentation">
-              <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Prijavnice</button>
-            </li>
-            {{-- <li class="nav-item" role="presentation">
-              <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Contact</button>
+              <button class="nav-link" id="application-table-tab" data-bs-toggle="tab" data-bs-target="#application-table-tab-pane" type="button" role="tab" aria-controls="application-table-tab-pane" aria-selected="false">Prijavnice</button>
             </li>
             <li class="nav-item" role="presentation">
+              <button class="nav-link" id="athletes-tab" data-bs-toggle="tab" data-bs-target="#athletes-tab-pane" type="button" role="tab" aria-controls="athletes-tab-pane" aria-selected="false">Natjecatelji</button>
+            </li>
+            {{-- <li class="nav-item" role="presentation">
               <button class="nav-link" id="disabled-tab" data-bs-toggle="tab" data-bs-target="#disabled-tab-pane" type="button" role="tab" aria-controls="disabled-tab-pane" aria-selected="false" disabled>Disabled</button>
             </li> --}}
           </ul>
           <div class="tab-content" id="myTabContent">
+            {{-- BASIC INFO TAB --}}
             <div class="tab-pane fade show active" id="basic-info-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
                 <x-basic-competition-info :competition="$competition">
                 </x-basic-competition-info>
             </div>
-            <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+            {{-- APPLICATION TABLE --}}
+            <div class="tab-pane fade" id="application-table-tab-pane" role="tabpanel" aria-labelledby="application-table-tab" tabindex="0">
                 <div class="container mt-3">
                     @livewire('application-form-table',[
                         'theme' => 'bootstrap-5',
@@ -38,8 +40,15 @@
                     ])
                 </div>
             </div>
-            {{-- <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">...</div>
-            <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">...</div> --}}
+            <div class="tab-pane fade" id="athletes-tab-pane" role="tabpanel" aria-labelledby="athletes-tab" tabindex="0">
+              <div class="container mt-3">
+                @livewire('athletes-in-comp-table',[
+                    'theme' => 'bootstrap-5',
+                    'compId' => $competition->id,
+                ])
+              </div>
+            </div>
+            {{-- <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">...</div> --}}
           </div>
         
     </div>
